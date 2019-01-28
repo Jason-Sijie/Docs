70c00c1fc687ec010afb346bc8207b4152062c38
Jason Yu <yusj.jason@gmail.com>
2019-01-28 15:47:13 -0800

    new two questions

---
+++ leetcode/48_Rotate_Image.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i<n/2; i++){
+            for(int j = 0; j<n-1-i-i; j++){
+                int t = matrix[n-1-i-j][i];
+                matrix[n-1-i-j][i] = matrix[n-1-i][n-1-i-j];
+                matrix[n-1-i][n-1-i-j] = matrix[i+j][n-1-i];
+                matrix[i+j][n-1-i] = matrix[i][i+j];
+                matrix[i][i+j] = t;
+            }
+        }
+    }
+};
